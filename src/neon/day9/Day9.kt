23cda5476fc4a048cd85b28b8093bf4fc5680e3d
Erik Norlander <e.olof.norlander@gmail.com>
2017-12-09 13:52:25 +0100

    day 9

---
+++ src/neon/day9/Day9.kt
@@ -0,0 +1,47 @@
+package neon.day9
+
+import java.io.File
+import java.util.*
+import kotlin.collections.ArrayList
+
+fun main(args: Array<String>) {
+    val stream = File("inputfiles/day9.txt").readLines()[0].asSequence()
+    val groups: Deque<Char> = ArrayDeque<Char>()
+    val scores: MutableList<Int> = ArrayList()
+    var insideGarbage = false
+    var ignoreNextChar = false
+    var nbrGarbageChars = 0
+    var depth = 0
+    stream.forEach {
+        if (ignoreNextChar) {
+            ignoreNextChar = false
+        } else if (it == '!') {
+            ignoreNextChar = true
+        } else if (insideGarbage) {
+            if (it == '>') {
+                insideGarbage = false
+            } else {
+                nbrGarbageChars++
+            }
+        } else { // not in garbage
+            when (it) {
+                '{' -> {
+                    groups.add(it)
+                    scores.add(++depth)
+                }
+                '}' -> {
+                    groups.pop()
+                    depth--
+                }
+                '<' -> {
+                    insideGarbage = true
+                }
+                else -> {
+
+                }
+            }
+        }
+    }
+    println(scores.sum())
+    println("nbr garbage chars: $nbrGarbageChars")
+}
