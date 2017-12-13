a807d7db34a17d24bedbebd6f1af07f7a305fd4f
Erik Norlander <e.olof.norlander@gmail.com>
2017-12-13 16:17:58 +0100

    use sequence instead, to speed up part2

--- src/neon/day13/Day13.kt
+++ src/neon/day13/Day13.kt
@@ -1,22 +1,34 @@
 package neon.day13
 
 import java.io.File
+import kotlin.coroutines.experimental.buildSequence
+import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
+    val time =measureTimeMillis { partTwo() }
+    println("Execution time: $time ms")
+}
+
+private fun partTwo() {
     val input = File("inputfiles/day13.txt").readLines().map {
         val arr = it.split(": ")
         Pair<Int, Int>(arr[0].trim().toInt(), arr[1].trim().toInt())
     }
     var result = -1
     var waitingTime = -1
-    while (result != 0) {
+    var caught = true
+    while (caught) {
         waitingTime++
-        result = input.fold(0) { cost, (layer, depth) ->
+        caught = input.asSequence().any { (layer, depth) ->
+            (layer + waitingTime) % (2 * depth - 2) == 0
+        }
+        /*result = input.fold(0) { cost, (layer, depth) ->
             val caughtCost = if ((layer + waitingTime) % (2 * depth - 2) == 0) {
                 Math.max(layer * depth, 1)
             } else 0
             caughtCost + cost
         }
+        */
     }
     println("The minimal waiting time is $waitingTime")
 }
